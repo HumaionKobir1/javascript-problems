5cb4863e795c702fa95b3d1b3c5b7d439c6f2a95
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-02 22:33:25 +0600

    Added => simple javascript swap problem solved

---
+++ swap.js
@@ -0,0 +1,24 @@
+let a = 5;
+let b = 7;
+// console.log('before swap: a =', a, 'b =', b);
+let temp = a;
+a = b;
+b = temp;
+// console.log('after swap: a =', a, 'b =', b);
+
+// or
+
+var x = 5;
+var y = 7;
+x = x + y;
+y = x - y;
+x = x - y;
+console.log('after swap: x =', x, 'y =', y);
+
+
+// or
+
+var p = 5;
+var q = 7;
+[p, q] = [q, p];
+console.log('after swap: p =', p, 'q =', q);
