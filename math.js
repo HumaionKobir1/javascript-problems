425958898c3ca24ffb3b21aeaee647d81cdce622
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-03 18:02:59 +0600

    Added => Math, abs, pow, round practice

---
+++ math.js
@@ -0,0 +1,25 @@
+const result = Math.pow(2, 5);
+console.log(result);
+
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.pow(num1 - num2) ;
+
+if(gap < 5){
+    console.log('you guys can be friends');
+}
+else{
+    console.log('you guys stay apart');
+}
+
+
+
+
+
+const Number = 2.55446;
+const fullNumber = Math.round(Number);
+console.log(fullNumber);
+
+// Practice the same way for ceil, flor, and random number
